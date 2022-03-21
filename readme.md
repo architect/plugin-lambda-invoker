3ad1add7dd44842699eeea2468bfe4ddfa2d0835
Ryan Block <rblock@gmail.com>
2022-03-20 20:13:49 -0700

    hbd!

---
+++ readme.md
@@ -0,0 +1,84 @@
+[<img src="https://assets.arc.codes/architect-logo-500b@2x.png" width=500>](https://www.npmjs.com/package/@architect/architect/plugin-lambda-invoker)
+
+## [`@architect/architect/plugin-lambda-invoker`](https://www.npmjs.com/package/@architect/architect/plugin-lambda-invoker)
+
+> Interactively invoke Lambdas in Architect Sandbox with arbitrary events
+
+[![GitHub CI status](https://github.com/architect/architect/plugin-lambda-invoker/workflows/Node%20CI/badge.svg)](https://github.com/architect/architect/plugin-lambda-invoker/actions?query=workflow%3A%22Node+CI%22)
+
+
+## Install
+
+Into your existing Architect project:
+
+```sh
+npm i @architect/architect/plugin-lambda-invoker --save-dev
+```
+
+Add the following to your Architect project manifest (usually `app.arc`):
+
+```arc
+@plugins
+architect/architect/plugin-lambda-invoker
+```
+
+
+## Usage
+
+While Sandbox is running, type `i` in your terminal to bring up the Lambda invocation menu. Then select the Lambda you'd like to invoke.
+
+By default, this plugin will populate your menu with all `@events`, `@queues`, `@scheduled`, and `@tables-streams`; you can limit this menu (or expand it with additional pragmas) by adding the following setting to a `pref[erence]s.arc` file:
+
+```arc
+@sandbox
+invoker http scheduled # This would populate @http + @scheduled Lambdas, and ignore all others
+```
+
+> Tip: you can navigate the invocation menu by typing numbers (zero-indexed)!
+
+
+### Invocation mocks
+
+By default, Lambdas are invokved with an empty payload (`{}`); if you'd like to invoke your Lambdas with arbitrary payloads, create a file containing invocation mocks.
+
+Invocation mock files live in your root with one of these filenames: `sandbox-invoke-mocks.json` or `sandbox-invoke-mocks.js`. These files should be structured like so:
+
+Assuming this project manifest:
+
+```arc
+@events
+background-task
+
+@queues
+analytics
+
+@scheduled
+backup-database
+```
+
+If you wanted to add one or more mocks to each of the three Lambdas above, create the following `sandbox-invoke-mocks.js` (or equivalent JSON) file with the format of `[pragmaName][lambdaName][mockName]`:
+
+```js
+module.exports = {
+  events: {
+    'background-task': {
+      'my-first-mock': { /* payload */ },
+      'another-mock': { /* payload */ },
+    }
+  },
+  queues: {
+    analytics: {
+      'one-more-mock': { /* payload */ },
+      'just-a-mock': { /* payload */ },
+    }
+  },
+  scheduled: {
+    'backup-database': {
+      'a-mock-for-this': { /* payload */ },
+      'the-last-mock': { /* payload */ },
+    }
+  },
+}
+```
+
+> Tip: when using `sandbox-invoke-mocks.js`, you can dynamically (synchronously) generate mocks on startup
---
+++ src/index.js
@@ -0,0 +1,120 @@
+let { join } = require('path')
+let { existsSync, readFileSync } = require('fs')
+let { updater } = require('@architect/utils')
+let { prompt } = require('enquirer')
+let colors = require('ansi-colors')
+let update = updater('Invoker')
+
+module.exports = {
+  sandbox: {
+    start: async ({ inventory: { inv }, invoke }) => {
+      update.status(`Event invoker started, select an event to invoke by pressing 'i'`)
+      let { cwd, preferences } = inv._project
+      let jsonMocks = join(cwd, 'sandbox-invoke-mocks.json')
+      let jsMocks = join(cwd, 'sandbox-invoke-mocks.js')
+
+      let pragmas = [ 'events', 'queues', 'scheduled', 'tables-streams' ]
+      let prefs = preferences?.sandbox?.invoker
+      if (prefs) {
+        if (Array.isArray(prefs)) pragmas = prefs
+        else if (typeof prefs === 'string') pragmas = [ prefs ]
+        else throw Error('Invalid @architect/plugin-lambda-invoker plugin preferences')
+      }
+
+      // Build out the availble event list
+      let events = {}
+      pragmas.forEach(pragma => {
+        if (inv[pragma]) inv[pragma].forEach(({ name }) => {
+          events[`@${pragma} ${name}`] = { pragma, name }
+        })
+      })
+      // Add a cancel option should one desire
+      events.cancel = ''
+
+      process.stdin.on('data', async function eventInvokeListener (input) {
+        input = String(input)
+        // Reset Enquirer's styles
+        let options = {
+          prefix: colors.white(colors.symbols.question),
+          styles: {
+            em: colors.cyan, // Clear underlines
+            danger: colors.red,
+            strong: colors.white,
+          }
+        }
+        if (input === 'i') {
+          if (Object.keys(events).length === 1) {
+            let none = 'No Lambdas found to invoke'
+            if (pragmas.length) update.status(none, `Using the following pragmas: @${pragmas.join(', @')}`)
+            else update.status(none)
+            return
+          }
+
+          let payload = {}
+          let mocks
+          let mockName = 'empty'
+
+          // Load invocation mocks
+          if (existsSync(jsonMocks)) {
+            mocks = JSON.parse(readFileSync(jsonMocks))
+          }
+          else if (existsSync(jsMocks)) {
+            // Make sure changes to mocks are always reflected
+            delete require.cache[require.resolve(jsMocks)]
+            // eslint-disable-next-line
+            mocks = require(jsMocks)
+          }
+
+          let { lambda } = await prompt({
+            type: 'select',
+            name: 'lambda',
+            numbered: true,
+            message: 'Which event do you want to invoke?',
+            hint: '\nYou can use numbers to change your selection',
+            choices: Object.keys(events),
+          }, options)
+          if (lambda === 'cancel') return end()
+          let { pragma, name } = events[lambda]
+
+          // Present options for mocks (if any)
+          if (mocks?.[pragma]?.[name]) {
+            let selection = await prompt({
+              type: 'select',
+              name: 'mock',
+              numbered: true,
+              message: 'Which mock do you want to invoke?',
+              choices: [ ...Object.keys(mocks[pragma][name]), 'empty' ],
+            }, options)
+            mockName = selection.mock
+            payload = mocks[pragma][name][mockName] || {}
+          }
+
+          // Wrap it up and invoke!
+          let msg = `Invoking @${pragma} ${name}`
+          msg += ` with ${mockName === 'empty' ? 'empty' : `'${mockName}'`} payload`
+          update.status(msg)
+          await invoke({ pragma, name, payload })
+          end()
+        }
+      })
+    },
+    end: async () => {
+      // Only remove our listener; removing Enquirer causes funky behavior
+      process.stdin.rawListeners('data').forEach(fn => {
+        if (fn.name === 'eventInvokeListener') {
+          process.stdin.removeListener('data', fn)
+        }
+      })
+      end()
+    }
+  }
+}
+
+// Necessary per Enquirer #326
+function end () {
+  if (process.stdin.isTTY) {
+    process.stdin.setRawMode(true)
+    process.stdin.setEncoding('utf8')
+    process.stdin.resume()
+  }
+}
